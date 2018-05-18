f5064716973460af997b4d15723112f40e7ea436
Niksior <heroes.VS.misio@gmail.com>
2018-05-18 15:29:53 +0200

    refractorrrr

--- laby/AstroWeather/app/src/main/java/com/example/a202246/astroweather/PlaceholderFragment.java
+++ laby/AstroWeather/app/src/main/java/com/example/a202246/astroweather/PlaceholderFragment.java
@@ -6,7 +6,6 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 public class PlaceholderFragment extends Fragment {
     private static final String ARG_SECTION_NUMBER = "section_number";
@@ -26,8 +25,6 @@ public class PlaceholderFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-        TextView textView = rootView.findViewById(R.id.section_label);
-        textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
         return rootView;
     }
 }
